c8892deb99a5d6d62116d2fbdb1495a64c60a979
Ahmed Hassan <70380307+aahmed3025@users.noreply.github.com>
2025-02-07 15:38:54 -0600

    setup
    setup everythin with installtion

---
+++ cms/cms/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-qyu5s-%^7n-9+6i6$dikw&d6h1lmt+%w^p9+1@0(vzq-i=h2z4"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
